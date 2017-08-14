69e4ee106b0692edfdd4adb1c96a675e5f9858c5
tnksss <robsons@alunos.utfpr.edu.br>
2017-08-14 20:44:59 -0300

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  def hello
+  	render html: "¡hello, world!"  	
+  end
+
+  def goodbye
+  	render html:"goodbye, world!"
+  end
+end
